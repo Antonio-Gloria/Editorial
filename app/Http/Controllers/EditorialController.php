c649972751d1378b600e65874308144a82fbd4aa
Antonio <2122100585@soy.utj.edu.mx>
2025-05-22 00:34:25 -0600

    Parte 2

---
+++ app/Http/Controllers/EditorialController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Editorial;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
+class EditorialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+   //Abre el formulario de captura de registros
+   return view('editorial.create');
+}
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+{
+//validación de campos requeridos
+$this->validate($request, [
+   'nombre' => 'required|min:5',
+   'domicilio' => 'required',
+   'correo' => 'required'
+]);
+
+
+$editorial = new Editorial();
+$editorial->nombre = $request->input('nombre');
+$editorial->domicilio = $request->input('domicilio');
+$editorial->correo = $request->input('correo');
+$editorial->status = 1;
+$editorial->save();
+return redirect()->route('editoriales.index')->with(array(
+   'message' => 'La Editorial se ha guardado correctamente'
+));
+}
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
